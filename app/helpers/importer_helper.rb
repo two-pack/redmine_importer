3150ca160a98c6b61f217bafb886abf61e24ae95
Phill MV <phillmv@okayfail.com>
2014-05-22 18:42:34 -0400

    Reformatted view, pushed unpack ops into helper.

---
+++ app/helpers/importer_helper.rb
@@ -0,0 +1,15 @@
+module ImporterHelper
+  def matched_attrs(column)
+    matched = ''
+    @attrs.each do |k,v| 
+      if v.to_s.casecmp(column.to_s.sub(" ") {|sp| "_" }) == 0 or k.to_s.casecmp(column.to_s) == 0
+        matched = v 
+      end 
+    end
+    matched
+  end
+
+  def force_utf8(str)
+    str.unpack("U*").pack('U*')
+  end
+end
