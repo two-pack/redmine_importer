40fef3a172cd28383c914cd1de5bfd9aaa6497a4
gc-olivierho <olivier.houdas@geoconcept.com>
2015-07-21 11:47:25 +0200

    versionning and readme customized

--- init.rb
+++ init.rb
@@ -4,9 +4,9 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_importer do
   name 'Issue Importer'
-  author 'Martin Liu / Leo Hourvitz / Stoyan Zhekov / Jérôme Bataille'
+  author 'Martin Liu / Leo Hourvitz / Stoyan Zhekov / Jérôme Bataille / Olivier Houdas'
   description 'Issue import plugin for Redmine.'
-  version '1.2.2'
+  version '1.3.0'
 
   project_module :importer do
     permission :import, :importer => :index
