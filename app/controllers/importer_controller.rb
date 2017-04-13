8109fbab35bdb751e7d5679f111da8c32bad17cc
Tatsuya Saito <twopackas@gmail.com>
2017-04-13 23:40:43 +0900

    Fixes error when verison custom fields which can be selected multi values.

--- app/controllers/importer_controller.rb
+++ app/controllers/importer_controller.rb
@@ -510,7 +510,7 @@ class ImporterController < ApplicationController
       value = row[@attrs_map[cf.name]]
       unless value.blank?
         if cf.multiple
-          h[cf.id] = process_multivalue_custom_field(issue, cf, value)
+          h[cf.id] = process_multivalue_custom_field(project, issue, cf, value, add_versions)
         else
           begin
             value = case cf.field_format
@@ -774,11 +774,10 @@ class ImporterController < ApplicationController
     activity_id
   end
 
-  def process_multivalue_custom_field(issue, custom_field, csv_val)
+  def process_multivalue_custom_field(project, issue, custom_field, csv_val, add_versions)
     csv_val.split(',').map(&:strip).map do |val|
       if custom_field.field_format == 'version'
-        version = version_id_for_name(project, val, add_versions)
-        version.id
+        version_id_for_name(project, val, add_versions)
       else
         custom_field.value_from_keyword(val, issue)
       end
